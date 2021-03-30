1f4ee4cbc625350dceabc30c9a9657fe5ed2489c
pramudyapolinema <1941720054@student.polinema.ac.id>
2021-03-30 09:00:13 +0700

    Menambahkan route ke controller resource barang

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\BarangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('barang', BarangController::class);
